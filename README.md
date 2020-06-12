857ebc037e4776cedc499ff0c0a708436836f2b2
Michael Minter <mothore@gmail.com>
2020-06-12 01:42:12 -0700

    v0.1.0

---
+++ README.md
@@ -0,0 +1,58 @@
+# Description
+
+`Transformer` is a software interface to the [Elgato Stream Deck](https://www.elgato.com/en/gaming/stream-deck).
+
+Also see, [Stream Deck SDK](https://developer.elgato.com/documentation/stream-deck/sdk/overview/).
+
+# Features
+
+- Code written in Javascript
+- Cross-platform (macOS, Windows)
+- Localized
+
+![](screenshot.png)
+
+# Installation
+
+In the [Release](./Release/) folder, you can find the file `com.codekindness.transformer.streamDeckPlugin`. If you double-click this file on your machine, Stream Deck will install the plugin.
+
+# Development
+
+You can use vanilla Javascript (ES2015, ES6 and even ES7 is okay to use) or you can even include/import a library of your choice (e.g. jQuery, VueJS, etc...).
+
+## Installation
+
+Quit the Stream Deck application.
+
+On macOS copy the contents of this directory to the Stream Deck plugins folder.
+
+```
+cp -r Sources/com.codekindness.transformer.sdPlugin ~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/
+```
+
+On Windows the plugins directory can be found here:
+
+```
+%appdata%\Elgato\StreamDeck\Plugins\
+```
+
+If you now launch the Stream Deck application, this plugin should appear in the `Custom` category in the Actions list.
+
+## Debugging
+
+### Debugging your Javascript plugin
+
+You can debug your Javascript plugin and/or Property Inspector using Google Chrome's web developer tools. In order to do so, you first need to enable the HTML remote debugger in Stream Deck.
+
+On macOS, you will need to run the following command line in the Terminal:
+```
+defaults write com.elgato.StreamDeck html_remote_debugging_enabled -bool YES
+```
+
+On Windows, you will need to add a DWORD `html_remote_debugging_enabled` with value `1` in the registry `@ HKEY_CURRENT_USER\Software\Elgato Systems GmbH\StreamDeck`.
+
+After you relaunch the Stream Deck app, you can open [http://localhost:23654](http://localhost:23654) in Chrome, where you will find a list of 'Inspectable pages' (plugins). Inspecting your plugin is pretty straightforward: Click the reverse DNS name of your plugin and Chrome shows it's developer tools, where you can further inspect used components and log messages.
+
+# Resources
+
+All icons are taken from [Icons8](https://icons8.com).
---
+++ Sources/com.codekindness.transformer.sdPlugin/js/index.js
@@ -0,0 +1,167 @@
+var websocket = null;
+var pluginUUID = null;
+var DestinationEnum = Object.freeze({"HARDWARE_AND_SOFTWARE": 0, "HARDWARE_ONLY": 1, "SOFTWARE_ONLY": 2})
+var timer;
+var counterAction = {
+  type: "com.codekindness.transformer.action",
+
+  // context: instance of an action.
+  onKeyDown: function (context, settings, coordinates, userDesiredState) {
+    timer = setTimeout(function () {
+      var updatedSettings = {};
+      updatedSettings["keyPressCounter"] = -1;
+
+      counterAction.SetSettings(context, updatedSettings);
+      counterAction.SetTitle(context, 0);
+    }, 1500);
+  },
+
+  onKeyUp: function (context, settings, coordinates, userDesiredState) {
+
+    clearTimeout(timer);
+
+    var keyPressCounter = 0;
+
+    if (settings != null && settings.hasOwnProperty('keyPressCounter')) {
+      keyPressCounter = settings["keyPressCounter"];
+    }
+
+    keyPressCounter++;
+
+    updatedSettings = {};
+    updatedSettings["keyPressCounter"] = keyPressCounter;
+
+    this.SetSettings(context, updatedSettings);
+
+    this.SetTitle(context, keyPressCounter);
+
+    this.transform(context, settings, coordinates);
+  },
+
+  // occurs when action appears in canvas area
+  onWillAppear: function (context, settings, coordinates) {
+    var keyPressCounter = 0;
+
+    if (settings != null && settings.hasOwnProperty('keyPressCounter')) {
+      keyPressCounter = settings["keyPressCounter"];
+    }
+
+    this.SetTitle(context, keyPressCounter);
+  },
+
+  transform: function (context, settings, coordinates) {
+    // var pasteText = document.querySelector("#output");
+    // pasteText.focus();
+    // document.execCommand("paste");
+    // console.log(pasteText.textContent);
+
+    let clipboard = 'clipboard text'; // navigator.clipboard.readText();
+    let regex = '.*';
+    let replacement = clipboard;
+    let result = clipboard;
+
+    if (settings != null && settings.hasOwnProperty('replaceInput')) {
+      regex = RegExp(settings.replaceInput);
+
+      replacement = clipboard.replace(regex, settings['replaceInput']);
+      result = settings['selectionTextInput'].replace(/\$\{[0-9]+\}/, replacement);
+    }
+    console.log(result);
+    return result;
+  },
+
+  SetTitle: function (context, keyPressCounter) {
+    console.log(keyPressCounter);
+    var json = {
+      "event": "setTitle",
+      "context": context,
+      "payload": {
+        "title": "" + keyPressCounter,
+        "target": DestinationEnum.HARDWARE_AND_SOFTWARE
+      }
+    };
+
+    websocket.send(JSON.stringify(json));
+  },
+
+  SetInputSettings: function (context, settings, payload) {
+    console.log(settings);
+    let results = {};
+
+    if (settings != null) {
+      results = settings;
+    }
+
+    results[payload.sdpi_collection.key] = payload.sdpi_collection.value;
+
+    this.SetSettings(context, results);
+  },
+
+  SetSettings: function (context, settings) {
+    var json = {
+      "event": "setSettings",
+      "context": context,
+      "payload": settings
+    };
+
+    websocket.send(JSON.stringify(json));
+  }
+};
+
+function connectElgatoStreamDeckSocket(inPort, inPluginUUID, inRegisterEvent, inInfo) {
+  pluginUUID = inPluginUUID
+
+  // Open the web socket
+  websocket = new WebSocket("ws://127.0.0.1:" + inPort);
+
+  function registerPlugin(inPluginUUID) {
+    var json = {
+      "event": inRegisterEvent,
+      "uuid": inPluginUUID
+    };
+
+    websocket.send(JSON.stringify(json));
+  }
+
+  websocket.onopen = function () {
+    // WebSocket is connected, send message
+    registerPlugin(pluginUUID);
+  };
+
+  websocket.onmessage = function (evt) {
+    console.log('websocket.onmessage', evt);
+    // Received message from Stream Deck
+    var jsonObj = JSON.parse(evt.data);
+    var event = jsonObj['event'];
+    var action = jsonObj['action'];
+    var context = jsonObj['context'];
+
+    if (event == "keyDown") {
+      var jsonPayload = jsonObj['payload'];
+      var settings = jsonPayload['settings'];
+      var coordinates = jsonPayload['coordinates'];
+      var userDesiredState = jsonPayload['userDesiredState'];
+      counterAction.onKeyDown(context, settings, coordinates, userDesiredState);
+    } else if (event == "keyUp") {
+      var jsonPayload = jsonObj['payload'];
+      var settings = jsonPayload['settings'];
+      var coordinates = jsonPayload['coordinates'];
+      var userDesiredState = jsonPayload['userDesiredState'];
+      counterAction.onKeyUp(context, settings, coordinates, userDesiredState);
+    } else if (event == "willAppear") {
+      var jsonPayload = jsonObj['payload'];
+      var settings = jsonPayload['settings'];
+      var coordinates = jsonPayload['coordinates'];
+      counterAction.onWillAppear(context, settings, coordinates);
+    } else if (event == "sendToPlugin") {
+      var jsonPayload = jsonObj['payload'];
+      var settings = jsonPayload['settings'];
+      var coordinates = jsonPayload['coordinates'];
+      counterAction.SetInputSettings(context, settings, jsonPayload);
+    }
+  };
+
+  websocket.onclose = function () {
+    // Websocket is closed
+  };
+}
